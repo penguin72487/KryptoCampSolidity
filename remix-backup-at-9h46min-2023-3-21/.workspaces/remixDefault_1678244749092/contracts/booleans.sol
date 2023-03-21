89e5e1bf38c27a1d331f470937ccbeb923f9290b
penguin72487 <agar.io6100@Gmail.com>
2023-03-21 13:30:57 +0800

    import remix whitelist treap

---
+++ remix-backup-at-9h46min-2023-3-21/.workspaces/remixDefault_1678244749092/contracts/booleans.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.4 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Booleans{
+    bool public isbool = 0;
+    constructor(){}
+}

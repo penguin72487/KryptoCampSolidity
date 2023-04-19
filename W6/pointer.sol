f9c3b212e3eca3de7e672485b7aacdc8291d3a0c
penguin72487 <agar.io6100@Gmail.com>
2023-04-19 10:24:24 +0800

    simple amm get price deploy

--- W6/pointer.sol
+++ W6/pointer.sol
@@ -1,32 +1,58 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.18 <0.9.0;
 
-contract Pointer{
-    uint256[] public pointer; // index is pointer, *p is value, pointer[0] is nullptr
+contract Node {
+    uint256 public index;
+    uint256 public value;
+
+    constructor(uint256 _index, uint256 _value) {
+        index = _index;
+        value = _value;
+    }
+
+    function delete_uint256() public {
+        index = 0;
+        value = type(uint256).max;
+    }
+
+    function setIndex(uint256 _index) public {
+        index = _index;
+    }
+
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+}
+
+contract Pointer {
+    Node[] public pointer;
     uint256[] public nullPointer;
+
     constructor() {
-        pointer.push(0);
+        pointer.push(new Node(0, type(uint256).max));
     }
-    function new_uint256(uint256 value) public returns (uint256){
-        if (nullPointer.length == 0){
-            pointer.push(value);
-            return pointer.length - 1;
-        }
-        else{
-            uint256 p = nullPointer[nullPointer.length - 1];
+
+    function new_uint256(uint256 value) public returns (uint256) {
+        uint256 p;
+        if (nullPointer.length > 0) {
+            p = nullPointer[nullPointer.length - 1];
             nullPointer.pop();
-            pointer[p] = value;
-            return p;
+            pointer[p].setIndex(pointer.length); // =
+            pointer[p].setValue(value); // =
+        } else {
+            p = pointer.length;
+            pointer.push(new Node(p, value));
         }
+        return p;
     }
-    function delete_uint256(uint256 p) public{
-        require((p != 0&&p<pointer.length), "Cannot delete nullptr");
-        if(p==pointer.length-1){
+    function delete_uint256(uint256 p) public {
+        require(p < pointer.length, "out of range");
+        if(p == pointer.length - 1) {
             pointer.pop();
-        }
-        else{
-            pointer[p] = 0;
+        } else {
+            pointer[p].delete_uint256();
             nullPointer.push(p);
         }
     }
+    
 }

06267a36202b036f86c06257615857ecad2d9dca
penguin72487 <agar.io6100@Gmail.com>
2023-04-22 10:32:44 +0800

    add src

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-
+import "../src/bignum.sol";
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+
     }
 }

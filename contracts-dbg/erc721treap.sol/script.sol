d82ae8c1c59adbb081a0cac0ca3bb69913f1bfce
penguin72487 <agar.io6100@Gmail.com>
2023-04-22 17:16:07 +0800

    remove

--- contracts-dbg/erc721treap.sol/script.sol
+++
@@ -1,9 +0,0 @@
-// // SPDX-License-Identifier: MIT
-// import "./erc721treap.sol.t.sol";
-
-// contract DbgEntry {
-//     constructor() {
-//         nft = new FanartNFT();
-//         nft.mint();
-//     }
-// }

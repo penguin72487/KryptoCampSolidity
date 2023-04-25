981a38fcb760366ffd67a78cfa4a1ee13c78b92e
penguin72487 <agar.io6100@Gmail.com>
2023-04-25 21:34:01 +0800

    V5 放棄

--- W6/simpleAMM/src/simpleAMM.sol
+++ W6/simpleAMM/src/simpleAMM.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 // V1 0xf60440f93a677AB6968E1Fd10cf8a6cE61941131
 // V2 0x8b175c421E9307F0365dd37bc32Dda5df95C4946
 // V3 0x3ea585565c490232b0379C7D3C3A9fC3fA5C9c0C
-// v4 
+// V4
 import "./erc20.sol";
 contract AMM {
     IERC20 public immutable token;
@@ -13,110 +13,39 @@ contract AMM {
     uint256 public reserve0;
     uint256 public reserve1;
 
-    uint256 public totalSupply; // total LP
+    uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-    mapping (address=>uint256) public au_LPindex;
-    mapping (uint256=>address) public ua_LPaddress;
-    int256[] public diff_LP;//  differences
-    uint256 public sumLP;// sigima diff_LP
- 
 
     constructor(address _token) {
         token = IERC20(_token);
-        diff_LP.push(0);
     }
 
     function _mint(address _to, uint256 _amount) private {
-        if(balanceOf[_to]==0)
-        {
-            au_LPindex[_to]=diff_LP.length;
-            ua_LPaddress[diff_LP.length]=_to;
-            diff_LP.push(0);
-        }
         balanceOf[_to] += _amount;
         totalSupply += _amount;
-        
     }
 
     function _burn(address _from, uint256 _amount) private {
-        balanceOf[_from] -= _amount/_shareBaseOf(_from);
+        balanceOf[_from] -= _amount;
         totalSupply -= _amount;
-        _removeLiquidityUser(_from);
-    }
-    function _shareBaseOf(address _user) internal view returns (uint256) {
-        uint256 base = 0;
-        for (uint256 i = 0; i < au_LPindex[_user]; i++) {
-            base += uint256(diff_LP[i]);
-        }
-        return base;
-    }
-    function _removeLiquidityUser(address _user) internal
-    {
-        require(balanceOf[_user]==0,"balance not empty!");
-        uint256 index = au_LPindex[_user];
-        uint256 last = diff_LP.length-2;
-        uint256 user_shareBase=_shareBaseOf(_user);
-        sumLP-=user_shareBase;
-        (index,au_LPindex[ua_LPaddress[last]]) = swap(index,au_LPindex[ua_LPaddress[last]]);
-        (ua_LPaddress[index],ua_LPaddress[au_LPindex[ua_LPaddress[last]]]) = swap(ua_LPaddress[index],ua_LPaddress[au_LPindex[ua_LPaddress[last]]]);
-        swap(diff_LP[diff_LP.length-1],diff_LP[last]);
-        diff_LP.pop();
-        diff_LP[index+1]-=int256(user_shareBase);
-        diff_LP[index]=int256(sumLP-user_shareBase);
-
-    }
-    function swap(uint256 a,uint256 b) public pure returns (uint256,uint256) {
-        uint256 temp = a;
-        a = b;
-        b = temp;
-        return (a,b);
-    }
-    function swap(int a,int b) public pure returns (int,int) {
-        int temp = a;
-        a = b;
-        b = temp;
-        return (a,b);
-    }
-    function swap(address a,address b) public pure returns (address,address) {
-        address temp = a;
-        a = b;
-        b = temp;
-        return (a,b);
     }
 
     function _update(uint256 _reserve0, uint256 _reserve1) private {
         reserve0 = _reserve0;
         reserve1 = _reserve1;
     }
-    function my_Share() public view returns (uint256) {
-        uint256 base = _shareBaseOf(msg.sender);
-        return base + balanceOf[msg.sender];
-    }
-    function all_Share() public view returns (uint256) {
-        return totalSupply;
-    }
-    function share_Of(address _user) public view returns (uint256) {
-        uint256 base = _shareBaseOf(_user);
-        return base + balanceOf[_user];
-    } 
 
-    function all_addShare() internal { //
-        totalSupply+=diff_LP.length-1;
-        sumLP+=diff_LP.length-1;
-        diff_LP[0]+=1;
-        diff_LP[diff_LP.length-1]-=1;
-    }
-    function swap(uint256 _amountIn) public payable returns (uint256 amountOut) {
+    function swap(uint256 _amountIn) external payable returns (uint256 amountOut) {
         require(msg.value == _amountIn, "ETH amount mismatch");
 
         uint256 amountInWithFee = (_amountIn * 997) / 1000;
         amountOut = (reserve1 * amountInWithFee) / (reserve0 + amountInWithFee);
-        all_addShare();
+
         token.transfer(msg.sender, amountOut);
 
         _update(address(this).balance, token.balanceOf(address(this)));
     }
-    function swapTokenForETH(uint256 _amountIn) public returns (uint256 amountOut) {
+    function swapTokenForETH(uint256 _amountIn) external returns (uint256 amountOut) {
         uint256 amountInWithFee = (_amountIn * 997) / 1000;
 
         amountOut = (reserve0 * amountInWithFee) / (reserve1 + amountInWithFee);
@@ -133,21 +62,8 @@ contract AMM {
 
         uint256 _amount0 = msg.value;
 
-        if (reserve0 + reserve1 > 0) {
-            if (reserve0 * _amount1 != reserve1 * _amount0) {
-                uint256 targetAmount0 = (reserve1 * _amount1) / reserve1;
-                uint256 targetAmount1 = (reserve0 * _amount0) / reserve0;
-
-                if (_amount0 < targetAmount0) {
-                    uint256 requiredTokenAmount = targetAmount1 - _amount1;
-                    uint256 amountOut = swapTokenForETH(requiredTokenAmount);
-                    _amount0 += amountOut;
-                } else {
-                    uint256 requiredEthAmount = targetAmount0 - _amount0;
-                    uint256 amountOut = swap(requiredEthAmount);
-                    _amount1 += amountOut;
-                }
-            }
+        if (reserve0 > 0 || reserve1 > 0) {
+            require(reserve0 * _amount1 == reserve1 * _amount0, "x / y != dx / dy");
         }
 
         if (totalSupply == 0) {
@@ -163,12 +79,11 @@ contract AMM {
 
         _update(address(this).balance, token.balanceOf(address(this)));
     }
-
-    function removeLiquidity(uint256 _share) external
-    {
-        uint256 share = share_Of(msg.sender);
-        require(share>=_share,"shares not enough");
-        _removeLiquidity(_share);
+    function removeLiquidity(uint256 _shares) external returns (uint256 amount0, uint256 amount1) {
+        return _removeLiquidity(balanceOf[msg.sender]);
+    }
+    function removeLiquidity(address _user,uint256 _shares) external returns (uint256 amount0, uint256 amount1) {
+        return _removeLiquidity(balanceOf[_user]);
     }
     function _removeLiquidity(uint256 _shares) internal returns (uint256 amount0, uint256 amount1) {
         amount0 = (_shares * address(this).balance) / totalSupply;
@@ -209,23 +124,3 @@ contract AMM {
 
 
 }
-// interface IERC20 {
-//     function totalSupply() external view returns (uint256);
-
-//     function balanceOf(address account) external view returns (uint256);
-
-//     function transfer(address recipient, uint256 amount) external returns (bool);
-
-//     function allowance(address owner, address spender) external view returns (uint256);
-
-//     function approve(address spender, uint256 amount) external returns (bool);
-
-//     function transferFrom(
-//         address sender,
-//         address recipient,
-//         uint256 amount
-//     ) external returns (bool);
-
-//     event Transfer(address indexed from, address indexed to, uint256 amount);
-//     event Approval(address indexed owner, address indexed spender, uint256 amount);
-// }
